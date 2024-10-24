39e6b367fa31f19d03a3410c0e8d9e6056a21e63
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-24 07:19:04 +0500

    figma changes implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import CallToAction from "@/components/CTA/CallToAction";
 import Faqs from "@/components/Faqs/Faqs";
-import FeaturesTools from "@/components/Features/FeaturesTools";
-import Footer from "@/components/footer/Footer";
 import Hero from "@/components/hero/Hero";
 import HowItWorks from "@/components/Howitworks/HowItWorks";
-import Insights from "@/components/insights/Insights";
-import Testimonials from "@/components/testimonials/Testimonials";
 
 export default function Home() {
 	return (
