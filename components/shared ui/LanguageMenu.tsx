2653791860317de720959879e5e33d5fbc4ea64d
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-31 22:10:43 +0500

    lang menu updated as per figma design

--- components/shared ui/LanguageMenu.tsx
+++ components/shared ui/LanguageMenu.tsx
@@ -4,6 +4,7 @@ import Russia from "@/public/flagrussia.svg";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
+import Check from "@/public/checked.svg";
 import { useState } from "react";
 
 const LanguageMenu = () => {
@@ -32,35 +33,70 @@ const LanguageMenu = () => {
 
 				<MenuItems
 					transition
-					anchor="bottom end"
-					className="w-36 divide-y-[0.3px]  text-[#0F1017] origin-top-right rounded-xl langMenu text-sm py-1 transition duration-100 ease-out  focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+					anchor={{ gap: "10px", to: "bottom start" }}
+					className="w-40 top-4  text-[#0F1017] origin-top-right rounded-xl langMenu text-sm py-1 transition duration-100 ease-out  focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
 				>
 					<MenuItem>
-						<button
-							onClick={() => setLang(Eng)}
-							className="group flex w-full items-center gap-2 rounded-lg p-3 data-[focus]:bg-white/10"
-						>
-							<Image src={Eng} alt="english" />
-							English
-						</button>
+						{({ focus }) => (
+							<button
+								onClick={() => setLang(Eng)}
+								className="group flex w-full items-center gap-2.5 rounded-lg p-3 data-[focus]:bg-white/10"
+							>
+								<Image src={Eng} alt="English" />
+								<span className={`${lang === Eng && "font-semibold"}`}>
+									English
+								</span>
+								<Image
+									src={Check}
+									alt="check"
+									className={`${
+										lang === Eng ? "inline-flex ml-auto" : "hidden"
+									}`}
+								/>
+							</button>
+						)}
 					</MenuItem>
 					<MenuItem>
-						<button
-							onClick={() => setLang(Brazil)}
-							className="group flex w-full items-center gap-2 rounded-lg p-3 data-[focus]:bg-white/10"
-						>
-							<Image src={Brazil} alt="Brazil" />
-							Brazilian
-						</button>
+						{({ focus }) => (
+							<button
+								onClick={() => setLang(Brazil)}
+								className="group flex w-full items-center gap-2.5 rounded-lg p-3 data-[focus]:bg-white/10"
+							>
+								<Image src={Brazil} alt="Brazil" />
+
+								<span className={`${lang === Brazil && "font-semibold"}`}>
+									Brazilian
+								</span>
+								<Image
+									src={Check}
+									alt="check"
+									className={`${
+										lang === Brazil ? "inline-flex ml-auto" : "hidden"
+									}`}
+								/>
+							</button>
+						)}
 					</MenuItem>
 					<MenuItem>
-						<button
-							onClick={() => setLang(Russia)}
-							className="group flex w-full items-center gap-2 rounded-lg p-3 data-[focus]:bg-white/10"
-						>
-							<Image src={Russia} alt="Russia" />
-							Russian
-						</button>
+						{({ focus }) => (
+							<button
+								onClick={() => setLang(Russia)}
+								className="group flex w-full items-center gap-2.5 rounded-lg p-3 data-[focus]:bg-white/10"
+							>
+								<Image src={Russia} alt="Russia" />
+
+								<span className={`${lang === Russia && "font-semibold"}`}>
+									Russian
+								</span>
+								<Image
+									src={Check}
+									alt="check"
+									className={`${
+										lang === Russia ? "inline-flex ml-auto" : "hidden"
+									}`}
+								/>
+							</button>
+						)}
 					</MenuItem>
 				</MenuItems>
 			</Menu>
