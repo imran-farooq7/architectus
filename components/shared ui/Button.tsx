850a5fe3f098e299ed120ecf4a939a023f46988d
Imran Farooq <imran_farooq7@hotmail.com>
2024-11-01 21:51:00 +0500

    mobile changes for btn group

--- components/shared ui/Button.tsx
+++ components/shared ui/Button.tsx
@@ -2,7 +2,7 @@ const Button = ({ text, disabled }: { text: string; disabled?: boolean }) => {
 	return (
 		<button
 			disabled={disabled}
-			className=" md:px-0 w-full min-w-[170px] max-w-44 text-nowrap text-base py-2.5 rounded-[10px] border-[2px] border-[#3D2278] font-medium bg-transparent text-[#3D2278] disabled:opacity-40"
+			className=" md:px-0 w-full min-w-40 max-w-44 text-nowrap text-base py-2.5 rounded-[10px] border-[2px] border-[#3D2278] font-medium bg-transparent text-[#3D2278] disabled:opacity-40"
 		>
 			{text}
 		</button>
